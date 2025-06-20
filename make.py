7d2a09fb8ab4eb3797d7c8caf9096e43bd489280
Devin Springborn <portalmaster55@gmail.com>
2025-06-20 16:32:43 -0400

    filter by only NoVtx and TrgNoVtx

--- make.py
+++ make.py
@@ -8,21 +8,19 @@ import glob
 import pickle
 from tqdm import tqdm
 
-MUON = "Vtx"
-TRIGGER = ["DST_PFScouting_DoubleMuonVtx", "DST_PFScouting_DoubleMuonNoVtx"]
-pickleString = "initial_studies_2025/histos_2025_TrgOR_Vtx.pkl"
-
-# MUON = "NoVtx"
+# MUON = "Vtx"
 # TRIGGER = ["DST_PFScouting_DoubleMuonVtx", "DST_PFScouting_DoubleMuonNoVtx"]
-# pickleString = "initial_studies_2025/histos_2025_TrgOR_NoVtx.pkl"
+# pickleString = "initial_studies_2025/histos_2025_TrgOR_Vtx.pkl"
+
+MUON = "NoVtx"
+TRIGGER = ["DST_PFScouting_DoubleMuonNoVtx"]
+pickleString = "initial_studies_2025/histos_2025_TrgNoVtx_NoVtx.pkl"
 
 
 ## Open the file
 selected_branches = []
 selected_branches += TRIGGER
-
 selected_branches.append("nScoutingMuon%sDisplacedVertex"%(MUON))
-
 selected_branches.append("nScoutingMuon%s"%(MUON))
 selected_branches.append("ScoutingMuon%s_pt"%(MUON))
 selected_branches.append("ScoutingMuon%s_eta"%(MUON))
@@ -31,18 +29,21 @@ selected_branches.append("ScoutingMuon%s_charge"%(MUON))
 
 
 indir = "/eos/user/f/fernance/DST-Muons/samples"
-files = glob.glob(f"{indir}/*.root")
-for f in tqdm(files, desc="Progress"):
-    events = uproot.open(f)["Events"].arrays(selected_branches, library="ak")
-    print(f"> Opened sample with {len(events)} events")
+outdir = "/afs/cern.ch/user/d/dspringb/private/CMSSW_15_0_5/src/new-scouting-analysis"
+isRereadingFullFile = True
+if isRereadingFullFile:
+    files = glob.glob(f"{indir}/*.root")
+    for f in tqdm(files, desc="Progress"):
+        events = uproot.open(f)["Events"].arrays(selected_branches, library="ak")
+        print(f"> Opened sample with {len(events)} events") # 1_763_889 events
 
-    ## Filter by trigger
-    #TrgOR
-    mask = ak.zeros_like(events[TRIGGER[0]], dtype=bool)
-    for trigger in TRIGGER:
-        mask = np.array(mask) | np.array(events[trigger])
-    events = events[mask]
-    
-    print(f"> {len(events)} events survive the filter")
-
-    print(events[0])
+        ## Filter by trigger
+        #TrgOR
+        mask = ak.zeros_like(events[TRIGGER[0]], dtype=bool)
+        for trigger in TRIGGER:
+            mask = np.array(mask) | np.array(events[trigger])
+        events = events[mask]
+        
+        print(f"> {len(events)} events survive the filter") # 500_206 events survive
+        print("~\nFirst event: events[0]")
+        print(events[0])
