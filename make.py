e7c071bcf41861c8522d00ef5bd9893311e5d928
Devin Springborn <portalmaster55@gmail.com>
2025-06-23 10:12:21 -0400

    Addtl filtering by num disp. vertices

--- make.py
+++ make.py
@@ -6,59 +6,81 @@ import mplhep
 import hist
 import glob
 import pickle
+import sys
 from tqdm import tqdm
 
-# MUON = "Vtx"
-# TRIGGER = ["DST_PFScouting_DoubleMuonVtx", "DST_PFScouting_DoubleMuonNoVtx"]
-# pickleString = "initial_studies_2025/histos_2025_TrgOR_Vtx.pkl"
+try:
+    if sys.argv[1].lower() == "--vtx":
+        MUON = "Vtx"
+        TRIGGER = ["DST_PFScouting_DoubleMuonVtx"]
+        pickleString = "zoom_2025/histos_2025_TrgVtx_Vtx.pkl"
+    elif sys.argv[1].lower() == "--novtx":
+        MUON = "NoVtx"
+        TRIGGER = ["DST_PFScouting_DoubleMuonNoVtx"]
+        pickleString = "zoom_2025/histos_2025_TrgNoVtx_NoVtx.pkl"
+    else:
+        print("Please supply only --vtx or --novtx.")
+        sys.exit(1)
+except IndexError:
+    print("Please supply a flag (--vtx, --novtx).")
+    sys.exit(1)
 
-MUON = "NoVtx"
-TRIGGER = ["DST_PFScouting_DoubleMuonNoVtx"]
-pickleString = "initial_studies_2025/histos_2025_TrgNoVtx_NoVtx.pkl"
-
-
-## Open the file
-selected_branches = []
-selected_branches += TRIGGER
-selected_branches.append("nScoutingMuon%sDisplacedVertex"%(MUON))
-selected_branches.append("nScoutingMuon%s"%(MUON))
-selected_branches.append("ScoutingMuon%s_pt"%(MUON))
-selected_branches.append("ScoutingMuon%s_eta"%(MUON))
-selected_branches.append("ScoutingMuon%s_phi"%(MUON))
-selected_branches.append("ScoutingMuon%s_charge"%(MUON))
-
-
+isRereadingFullFile = False
 indir = "/eos/user/f/fernance/DST-Muons/samples"
 outdir = "/afs/cern.ch/user/d/dspringb/private/CMSSW_15_0_5/src/new-scouting-analysis"
-isRereadingFullFile = False
-if isRereadingFullFile:
-    files = glob.glob(f"{indir}/*.root")
-    pickleIndex = 0
-    for f in tqdm(files, desc="Progress"):
-        events = uproot.open(f)["Events"].arrays(selected_branches, library="ak")
-        print(f"> Opened sample with {len(events)} events") # 1_763_889 events
+try:
+    if sys.argv[2].lower() == "--reread":
+        isRereadingFullFile = True
+except IndexError: # if --reread not specified, assume using same file.
+    print("Using stored data from "+ outdir + "/large_pickles/events[%s]Pickle.pkl"%(MUON))
+finally:
+    if isRereadingFullFile:
+        ## Open the file
+        selected_branches = []
+        selected_branches += TRIGGER
+        selected_branches.append("nScoutingMuon%sDisplacedVertex"%(MUON))
+        selected_branches.append("nScoutingMuon%s"%(MUON))
+        selected_branches.append("ScoutingMuon%s_pt"%(MUON))
+        selected_branches.append("ScoutingMuon%s_eta"%(MUON))
+        selected_branches.append("ScoutingMuon%s_phi"%(MUON))
+        selected_branches.append("ScoutingMuon%s_charge"%(MUON))
+        files = glob.glob(f"{indir}/*.root")
+        pickleIndex = 0
+        for f in tqdm(files, desc="Progress"):
+            events = uproot.open(f)["Events"].arrays(selected_branches, library="ak")
+            print(f"> Opened sample with {len(events)} events") # 1_763_889 events
 
-        ## Filter by trigger
-        #TrgOR
-        mask = ak.zeros_like(events[TRIGGER[0]], dtype=bool)
-        for trigger in TRIGGER:
-            mask = np.array(mask) | np.array(events[trigger])
-        events = events[mask]
-        
-        print(f"> {len(events)} events survive the filter") # 500_206 events survive from Vtx TrgOR
-        # 356_664 events survive NoVtx TrgNoVtx
-        print("~\nFirst event: events[0]")
-        print(events[0])
-        with open (outdir+"/large_pickles/events[%s]Pickle.pkl"%(MUON), "wb") as pickleOut:
-            pickle.dump(events, pickleOut)
-        pickleIndex += 1
-else: # if not rereading the original unfiltered file
-    with open (outdir+"/large_pickles/events%sPickle.pkl"%(MUON), "rb") as pickleIn:
-        events = pickle.load(pickleIn)
-    print(f"> Opened filtered sample with {len(events)} events") # 500_206 events Vtx TrgOR, # 356_664 events NoVtx TrgNoVtx
+            ## Filter by trigger
+            #TrgOR
+            mask = ak.zeros_like(events[TRIGGER[0]], dtype=bool)
+            for trigger in TRIGGER:
+                mask = np.array(mask) | np.array(events[trigger])
+            events = events[mask]
+            
+            print(f"> {len(events)} events survive the filter") # 500_206 events survive from Vtx TrgOR
+            # 356_664 events survive NoVtx TrgNoVtx
+            print("~\nFirst event: events[0]")
+            print(events[0])
+            with open (outdir+"/large_pickles/events[%s]Pickle.pkl"%(MUON), "wb") as pickleOut:
+                pickle.dump(events, pickleOut)
+            pickleIndex += 1
+    else: # if not rereading the original unfiltered file
+        with open (outdir+"/large_pickles/events%sPickle.pkl"%(MUON), "rb") as pickleIn:
+            events = pickle.load(pickleIn)
+        print(f"> Opened filtered sample with {len(events)} events") # 500_206 events Vtx TrgOR, # 356_664 events NoVtx TrgNoVtx
 
-## Filter by number of muons
-events = events[ events["nScoutingMuon%s"%(MUON)] > 1]
+    ## Filter by number of muons
+    events = events[ events["nScoutingMuon%s"%(MUON)] > 1]
+    print(f"> Events with 2+ muons: {len(events)}") # 500_206 events Vtx TrgOR, # 356_664 events NoVtx TrgNoVtx
+    # print(ak.fields(events[0])) # get list of fields again since i forgot
+    # print(events[0]["nScoutingMuon%sDisplacedVertex"%(MUON)])
 
-print(ak.fields(events[0])) # get list of fields again since i forgot
-print(events[0]["nScoutingMuon%sDisplacedVertex"%(MUON)])
+    ## Filter by having a displaced vertex reconstruction
+    events = events[events["nScoutingMuon%sDisplacedVertex"%(MUON)] > 0]
+    print(f"> Events with a displaced vertex reco: {len(events)}") 
+    
+    ## Print all charges as a test
+    for i in tqdm(range(len(events))):
+        print(events["ScoutingMuon%s_charge"%(MUON)][i])
+        print(events["nScoutingMuon%s"%(MUON)])
+        print("\n")
