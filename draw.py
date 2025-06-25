f41aa0f3d3c6e6ca1d54f0aad20042225ef7887b
Devin Springborn <portalmaster55@gmail.com>
2025-06-25 16:21:39 -0400

    Remove draw code, do curve fit test

--- draw.py
+++ draw.py
@@ -34,6 +34,32 @@ with open (outdir+"/large_pickles/events%sLxyPickle.pkl"%(MUON), "rb") as pickle
     h_lxy_peak = pickle.load(pickleIn)
     h_lxy_sidebands = pickle.load(pickleIn)
 
+
+
+# Fit an exponential to the two curves
+''' Definition from other file:
+h_lxy_sidebands = hist.new.Reg(100, lxy_range[0], lxy_range[1], name="lxy_sidebands", label="lxy_sidebands").Double()
+h_lxy_peak = hist.new.Reg(100, lxy_range[0], lxy_range[1], name="lxy_peak", label="lxy_peak").Double()
+'''
+from scipy.optimize import curve_fit
+
+def func(x, a, b):
+    return a * np.exp(-b*x)
+
+peak_bin_values = h_lxy_peak.values()
+peak_bin_centers = h_lxy_peak.axes[0].centers
+sidebands_bin_values = h_lxy_sidebands.values()
+sidebands_bin_centers = h_lxy_sidebands.axes[0].centers
+
+pk_param, pk_param_cov = curve_fit(func, peak_bin_centers, peak_bin_values)
+sb_param, sb_param_cov = curve_fit(func, sidebands_bin_centers, sidebands_bin_values)
+
+print("Peak params:", pk_param)
+print("Sideband params:", sb_param)
+
+
+
+'''
 plt.style.use(hep.style.CMS)
 fig, ax = plt.subplots(figsize=(10,8))
 hep.cms.label("Preliminary", data=True, year='2025', com='13.6', ax=ax, loc=2)
@@ -82,4 +108,4 @@ ax.set_ylabel("Number of events")
 ax.set_yscale("log")
 ax.legend
 fig.savefig("img/hist_lxy_pk_side_%s.png"%(MUON), dpi=300)
-
+'''
